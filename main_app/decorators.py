be18ab9229d2e47d7c44a2baf5866c4871ae5ae3
Mac User <macuser@BlackBox.local>
2019-03-19 12:36:39 -0500

    3/19/19: template updates

--- main_app/decorators.py
+++ main_app/decorators.py
@@ -1,32 +1,6 @@
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.decorators import user_passes_test
 
-<<<<<<< HEAD
-def student_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login')
-# Decorator for views that checks that the loggin in user is a student,
-# redirects to the log-in page if necessary
-actual_decorator = user_passes_test(
-    lambda u: u.is_active and u.is_student,
-    login_url = login_url,
-    redirect_field_name=redirect_field_name
-)
-if function:
-    return actual_decorator(function)
-return actual_decorator
-
-def teacher_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
-    #decorator for views that checks that the logged in user is a teacher,
-    # redirects to the log-in page if necessary.
-
-    actual_decorator = user_passes_test(
-        lambda u: u.is_active and u.is_teacher,
-        login_url=login_url,
-        redirect_field_name=redirect_field_name
-    )
-    if function:
-        return actual_decorator(function)
-    return actual_decorator
-=======
 # def student_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login')
 # # Decorator for views that checks that the loggin in user is a student,
 # # redirects to the log-in page if necessary
@@ -51,6 +25,5 @@ def teacher_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, log
 #     if function:
 #         return actual_decorator(function)
 #     return actual_decorator
->>>>>>> 63973a88048828ad4a0b8b416d31834e1eecb55c
 
 
--- main_app/models.py
+++ main_app/models.py
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-from django.contrib.auth.models import AbstractUser
-from django import forms
-
-# class CustomUser(AbstractUser):
-#     is_instructor = models.BooleanField(default=False)
-#     first_name = forms.CharField(max_length=30, required=True, label = "First Name")
-#     last_name = forms.CharField(max_length=30, required=True, label = "Last Name")
-#     email = forms.EmailField(max_length=256, required=True, label = "E-mail")
-#     location = forms.CharField(max_length=30, required=True, label = "Location")
-#     class_start_date = forms.CharField(max_length=30, required=True, label = "Start Date")
-#     class_type = forms.CharField(max_length=30, required=True, label = "Class Type")
-#     def __str__(self):
-#         return self.email
-=======
 from django.contrib.auth.models import User
 from django.db import models
 from django.db import models
@@ -25,4 +10,3 @@ class Instructor(models.Model):
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
->>>>>>> 63973a88048828ad4a0b8b416d31834e1eecb55c
